70be556eb1c6059f09c9f7e05a172e73a6292b03
kettunenj <juhani.kettunen@cgi.com>
2020-01-22 15:21:18 +0200

    Added environment variables and resource policies to lamdas. Lambda now saves raw metadata json files to s3 work bucket

--- Velho-analytics/lambdas/velhometadata/src/main/java/com/vayla/lambda/velho/metadata/LambdaFunctionHandler.java
+++ Velho-analytics/lambdas/velhometadata/src/main/java/com/vayla/lambda/velho/metadata/LambdaFunctionHandler.java
@@ -1,6 +1,8 @@
 package com.vayla.lambda.velho.metadata;
 
+import java.util.List;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -9,19 +11,18 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.vayla.lambda.velho.metadata.auth.AWS4SignerBase;
 import com.vayla.lambda.velho.metadata.auth.AWS4SignerForAuthorizationHeader;
 import com.vayla.lambda.velho.metadata.utils.HttpUtils;
 
 public class LambdaFunctionHandler implements RequestHandler<Object, String> {
-	private AmazonS3 s3 = AmazonS3Client.builder().withRegion(Regions.EU_CENTRAL_1).build();
-	static final String velhoHost = "api.stg.velho.vayla.fi";//System.getenv("velhoHost"); 
-	static final String velhoAPI = "https://api.stg.velho.vayla.fi/v1/kohdeluokat"; //System.getenv("velhoAPI");
-	static final String velhoRegion = "eu-central-1"; //System.getenv("velhoRegion"); 
+	private AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.EU_CENTRAL_1).build();
+	static final String velhoHost = System.getenv("velhoHost"); //"api.stg.velho.vayla.fi";
 	static final String velhoService = "execute-api"; // AWS API Gateway
 	static final String velhoAccessKey = System.getenv("velhoAccessKey");
 	static final String velhoSecretKey = System.getenv("velhoSecretKey");
+	static final String velhoWorkBucket = System.getenv("workBucket");
 	LambdaLogger logger;
 
 	public LambdaFunctionHandler() {
@@ -41,21 +42,41 @@ public class LambdaFunctionHandler implements RequestHandler<Object, String> {
 	public String handleRequest(Object event, Context context) {
 		//this.logger = context.getLogger();
 		log("## Received event: " + event);
+		//TODO: tuleeko metadatakutsut eventissa, vai listataanko valmiiksi ja kaydaan lapi?
+		List<URL> urlList = new ArrayList<URL>();
 
 		try {
-			// 1: luo headerit ja allekirjoitusavain 
-			AWS4SignerForAuthorizationHeader signer = new AWS4SignerForAuthorizationHeader(new URL(velhoAPI), "GET", velhoService, "eu-central-1");
-			Map<String, String> headers = new HashMap<String, String>();
-			Map<String, String> queryParams = new HashMap<String, String>();
-			String authHeader = signer.computeSignature(headers, queryParams, AWS4SignerBase.EMPTY_BODY_SHA256, velhoAccessKey, velhoSecretKey);
-			log(authHeader);
-			headers.put("Authorization", authHeader);
+			urlList.add(new URL("https://api.stg.velho.vayla.fi/v1/kohdeluokat"));
+			urlList.add(new URL("https://api.stg.velho.vayla.fi/v1/nimikkeistot"));
+			urlList.add(new URL("https://api.stg.velho.vayla.fi/v1/nimikkeisto/varustetiedot/kaidetyyppi"));
+			urlList.add(new URL("https://api.stg.velho.vayla.fi/v1/nimikkeisto/varustetiedot/materiaali"));
+			urlList.add(new URL("https://api.stg.velho.vayla.fi/v1/nimikkeisto/varustetiedot/kaidepylvastyyppi"));
+			urlList.add(new URL("https://api.stg.velho.vayla.fi/v1/nimikkeisto/varustetiedot/kaidenollaamatta"));
+			urlList.add(new URL("https://api.stg.velho.vayla.fi/v1/nimikkeisto/varustetiedot/lisatehtava"));
+			urlList.add(new URL("https://api.stg.velho.vayla.fi/v1/nimikkeisto/yleiset/jarjestelmakokonaisuus"));
 			
-			// 2: kutsu velhon rajapintaa
-			String jsonString = HttpUtils.invokeHttpRequest(new URL(velhoAPI), "GET", headers, null);
-			log("---------- RESPONSE ----------");
-			log(jsonString);
-			log("------------------------------");
+			for(URL myUrl : urlList) {
+				// 1: luo headerit ja allekirjoitusavain 
+				AWS4SignerForAuthorizationHeader signer = new AWS4SignerForAuthorizationHeader(myUrl, "GET", velhoService, "eu-central-1");
+				Map<String, String> headers = new HashMap<String, String>();
+				Map<String, String> queryParams = new HashMap<String, String>();
+				String authHeader = signer.computeSignature(headers, queryParams, AWS4SignerBase.EMPTY_BODY_SHA256, velhoAccessKey, velhoSecretKey);
+				log(authHeader);
+				headers.put("Authorization", authHeader);
+				
+				// 2: kutsu velhon rajapintaa jokaiselle metadata jsonille
+				String jsonString = HttpUtils.invokeHttpRequest(myUrl, "GET", headers, null);
+				log("---------- RESPONSE ----------");
+				log(jsonString);
+				log("------------------------------");
+				
+				// 3: tallenna buckettiin
+				String key = getS3KeyFromURL(myUrl);
+				log("## recieved key");
+				log(key);
+				log("## key end");
+				saveJSONToS3(key, jsonString);
+			}
 
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -64,5 +85,30 @@ public class LambdaFunctionHandler implements RequestHandler<Object, String> {
 		}
 		
 		return "OK";
+	}
+	
+	static String getS3KeyFromURL(URL endpoint) {
+		if ( endpoint == null ) {
+            throw new IllegalArgumentException("URL can't be null");
+        }
+        String path = endpoint.getPath();
+        if ( path == null || path.isEmpty() ) {
+            throw new IllegalArgumentException("URL must have a path");
+        }
+        // drop the leading separator for s3 key
+        if (path.startsWith("/")) {
+        	path = path.substring(1);
+        }
+        
+		return path+".json";
+	}
+	
+	void saveJSONToS3(String key, String json) {
+		log("************************************************");
+        log("*        Executing       'saveJSONToS3'        *");
+        log("************************************************");
+                
+        s3.putObject(velhoWorkBucket, key.toString(), json);
+        
 	}
 }
--- Velho-analytics/src/main/java/com/vayla/VelhoAnalyticsStack.java
+++ Velho-analytics/src/main/java/com/vayla/VelhoAnalyticsStack.java
@@ -2,22 +2,36 @@ package com.vayla;
 
 //import com.sun.tools.javac.util.Dependencies;
 import java.io.File;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import software.amazon.awscdk.core.Construct;
 import software.amazon.awscdk.core.Duration;
+import software.amazon.awscdk.core.SecretValue;
 import software.amazon.awscdk.core.Stack;
 import software.amazon.awscdk.core.StackProps;
-import software.amazon.awscdk.services.lambda.*;
-import software.amazon.awscdk.services.s3.*;
+import software.amazon.awscdk.services.iam.Effect;
+import software.amazon.awscdk.services.iam.PolicyStatement;
+import software.amazon.awscdk.services.lambda.Code;
+import software.amazon.awscdk.services.lambda.Function;
+import software.amazon.awscdk.services.s3.Bucket;
+import software.amazon.awscdk.services.s3.BucketEncryption;
+import software.amazon.awscdk.services.s3.BucketProps;
+import software.amazon.awscdk.services.s3.NotificationKeyFilter;
+import software.amazon.awscdk.services.s3.notifications.LambdaDestination;
+import software.amazon.awscdk.services.secretsmanager.Secret;
 import software.amazon.awscdk.services.sns.Topic;
-import software.amazon.awscdk.services.sns.subscriptions.SqsSubscription;
-import software.amazon.awscdk.services.sns.subscriptions.EmailSubscription;
 import software.amazon.awscdk.services.sqs.Queue;
 import software.amazon.awscdk.services.stepfunctions.Activity;
 import software.amazon.awscdk.services.stepfunctions.Chain;
 import software.amazon.awscdk.services.stepfunctions.StateMachine;
 import software.amazon.awscdk.services.stepfunctions.Task;
 import software.amazon.awscdk.services.stepfunctions.tasks.InvokeActivity;
-import software.amazon.awscdk.services.s3.notifications.*;
 
 public class VelhoAnalyticsStack extends Stack {
         public VelhoAnalyticsStack(final Construct parent, final String id) {
@@ -34,6 +48,7 @@ public class VelhoAnalyticsStack extends Stack {
                                 .build();
 
                 Bucket landingBucket = new Bucket(this, "velholandingbucket", s3BucketProps);
+                Bucket workBucket = new Bucket(this, "velhoworkbucket", s3BucketProps);
 
                 final Function evenPasserLambda = Function.Builder.create(this, "EventPasser")
                                 .functionName("VelhoLandingBucketEventPasser").timeout(Duration.minutes(5))
@@ -43,12 +58,47 @@ public class VelhoAnalyticsStack extends Stack {
                                 .runtime(software.amazon.awscdk.services.lambda.Runtime.JAVA_8)
                                 .handler("com.vayla.Lambdas.eventpasser.EventPass").build();
 
+                // Metadataloderin ymparistomuuttujat 
+                // TODO: access ja secret key secrets managerista
+                SecretValue velhokeys = Secret.fromSecretArn(this, "velhoaccess", "arn:aws:secretsmanager:eu-central-1:426182641979:secret:stg/velho/api-XFpyMk").getSecretValue();
+                Map<String, String> secretMap = new HashMap<String, String>();
+                ObjectMapper objectMapper = new ObjectMapper();
+                try {
+					secretMap = objectMapper.readValue(velhokeys.toString(), HashMap.class);
+				} catch (JsonMappingException e1) {
+					// TODO Auto-generated catch block
+					System.err.println("Input could not be mapped to JSON");
+					e1.printStackTrace();
+				} catch (JsonProcessingException e1) {
+					// TODO Auto-generated catch block
+					System.err.println("Input could not be parsed to JSON");
+					e1.printStackTrace();
+				}
+                
+                Map<String, String> environment = new HashMap<String, String>();
+                environment.put("velhoHost", "api.stg.velho.vayla.fi");
+                environment.put("workBucket", workBucket.getBucketName());
+                // lisataan velho api tunnukset sellaisenaan secrets managerista
+                for(String i : secretMap.keySet()) {
+                	environment.put(i, secretMap.get(i));
+                }
+                
                 final Function getMetadataLambda = Function.Builder.create(this, "MetadataLoaderLambda")
-                                .functionName("VelhoMetadataLoader").timeout(Duration.minutes(5))
+                                .functionName("VelhoMetadataLoader").timeout(Duration.minutes(5)).memorySize(1024)
                                 .code(Code.fromAsset("lambdas" + File.separator + "velhometadata" + File.separator
                                                 + "target" + File.separator + "velho.metadata-1.0.0.jar"))
                                 .runtime(software.amazon.awscdk.services.lambda.Runtime.JAVA_8)
+                                .environment(environment)
                                 .handler("com.vayla.lambda.velho.metadata.LambdaFunctionHandler").build();
+                // lisataan kayttooikeudet workbuckettiin
+                // TODO: korjattava, nyt on kaikki oikeudet kaikkiin resursseihin !!
+                getMetadataLambda.addToRolePolicy(PolicyStatement.Builder.create()
+                        .effect(Effect.ALLOW)
+                        .actions(Arrays.asList("*"))
+                        //.resources(Arrays.asList(workBucket.getBucketArn()))
+                        .resources(Arrays.asList("*"))
+                        .build());
+              
 
                 final Queue queue = Queue.Builder.create(this, "VelhoAnalyticsQueue")
                                 .visibilityTimeout(Duration.seconds(300)).build();
